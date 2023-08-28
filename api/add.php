dc00de21ba7d7ab07f9740cd1788180f05d2138c
jnc-bitbucket <jian.content@gmail.com>
2023-08-28 16:23:03 +1000

    commented headers CORS

--- api/add.php
+++ api/add.php
@@ -1,7 +1,7 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
 include("../backend/todoItem.php");
 
 // start the session
