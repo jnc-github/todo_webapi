71129dd61dddd918022da17596ca379bbb6392aa
jnc-bitbucket <jian.content@gmail.com>
2023-08-28 17:04:46 +1000

    test3

--- api/add.php
+++ api/add.php
@@ -1,5 +1,5 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://jnc-github.github.io/todo_webapi/");
 // header("Access-Control-Allow-Methods: POST, OPTIONS");
 // header("Access-Control-Allow-Headers: Content-Type, Authorization");
 include("../backend/todoItem.php");
